d847faadb01ee83af3f261e8e07522839221594a
Guy Leroy <g.m.leroy@outlook.com>
2020-02-08 15:10:21 +0000

    Keyboard controller and gitignore

---
+++ keyboard_control.py
@@ -0,0 +1,16 @@
+import keyboard
+import argparse
+
+def press_keys(key_string):
+    keys = key_string.split()
+    for key in keys:
+        try:
+            keyboard.press(key)
+        except:
+            print(key, "does not exist.")
+
+if __name__== "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--keys', help='One or multiple keys to press (key names can be space, left, a, ...)')
+    args = parser.parse_args()
+    press_keys(args.keys)
