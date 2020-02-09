ef6d85abf9ae501d9a24ea1282bbd392950854c5
Guy Leroy <g.m.leroy@outlook.com>
2020-02-09 08:46:29 +0000

    http server

---
+++ client.py
@@ -0,0 +1,41 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import logging
+import json
+import requests
+
+
+from msg_handlers.keyboard_control import KeyboardController
+
+class S(BaseHTTPRequestHandler):
+
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        print("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        #print("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+        #        str(self.path), str(self.headers), post_data.decode('utf-8'))
+        data = json.loads(post_data.decode('utf-8'))
+        key = data["key"]
+        print(key)
+        KeyboardController().press_keys(key)
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+
+
+def run(server_class=HTTPServer, handler_class=S):
+    server_address = ('', 8000)
+    print('Starting httpd...')
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+run()
