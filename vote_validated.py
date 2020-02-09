493feb6c8264466f2c9452d7d2c078fc5a1b7c1e
Remi <rku16@ic.ac.uk>
2020-02-09 11:28:46 +0000

    Voter

---
+++ vote_validated.py
@@ -0,0 +1,36 @@
+
+
+class ValidateVote:
+    """Constructs the onboarding message and stores the state of which tasks were completed."""
+
+    # TODO: Create a better message builder:
+    # https://github.com/slackapi/python-slackclient/issues/392
+    # https://github.com/slackapi/python-slackclient/pull/400
+    VOTED_BLOCK = lambda vote: {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": (
+                f"Bip boop... chat has voted to do {vote}"
+            ),
+        },
+    }
+    DIVIDER_BLOCK = {"type": "divider"}
+
+    def __init__(self, channel, vote):
+        self.channel = channel
+        self.vote = vote
+        self.username = "pythonboardingbot"
+        self.icon_emoji = ":robot_face:"
+        self.timestamp = ""
+
+    def get_message_payload(self):
+        return {
+            "ts": self.timestamp,
+            "channel": self.channel,
+            "username": self.username,
+            "icon_emoji": self.icon_emoji,
+            "blocks": [
+                self.WELCOME_BLOCK(self.vote)
+            ],
+        }
